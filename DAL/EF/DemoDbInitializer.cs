96c89933585d6fd43f5b9b8389ef699f47bd811b
alexandermatheussen <alexander.matheussen@student.kdg.be>
2019-04-02 13:05:36 +0200

    initializer updates

--- DAL/EF/DemoDbInitializer.cs
+++ DAL/EF/DemoDbInitializer.cs
@@ -48,7 +48,7 @@ namespace DAL.EF
             #region TestProject 1
             Project p1 = new Project()
             {
-                description = "We are thinking about build a new playground in the middle of central park",
+                description = "We are thinking about building a new playground in the middle of central park",
                 endDate = new DateTime(2019, 3,9),
                 startDate = new DateTime(2019,4,10),
                 name = "Playground"
@@ -58,19 +58,17 @@ namespace DAL.EF
             Questionnaire questionnaire1 = new Questionnaire()
             {
                 name = "Safety",
-                questionAmount = 3,
+                questionAmount = 4,
                 project = p1,
                 confirmed = true,
-                //iotSetups = new List<IotSetup>(),
                 questions = new List<Question>()
             };
             Questionnaire questionnaire2 = new Questionnaire()
             {
                 name = "Noise",
-                questionAmount = 2,
+                questionAmount = 3,
                 project = p1,
                 confirmed = false,
-               // iotSetups = new List<IotSetup>(),
                 questions = new List<Question>()
             };
             p1.questionnaires = new List<Questionnaire>() { questionnaire1, questionnaire2 };
@@ -81,16 +79,31 @@ namespace DAL.EF
                 question = "Do we need to build a fence around the playground?",
                 questionnaire = questionnaire1,
                 questionType = QuestionType.RADIO_BUTTON,
+                IotSetup = null,
                 options = new List<Option>()
             };
             Option o1 = new Option() {option = "Yes", question = q1};
             Option o2 = new Option() {option = "No", question = q1};
             q1.options = new List<Option>() { o1, o2 };
+            Question question1 = new Question()
+            {
+                question = "How big should the playground be?",
+                questionnaire = questionnaire1,
+                questionType = QuestionType.RADIO_BUTTON,
+                IotSetup = null,
+                options = new List<Option>()
+            };
+            Option option1 = new Option() {option = "50m²", question = question1};
+            Option option2 = new Option() {option = "60m²", question = question1};
+            Option option3 = new Option() {option = "80m²", question = question1};
+            Option option4 = new Option() {option = "100m²", question = question1};
+            question1.options = new List<Option>() { option1, option2, option3, option4 };
             Question q2 = new Question()
             {
                 question = "Where should we build the playground",
                 questionnaire = questionnaire1,
                 questionType = QuestionType.DROPDOWN,
+                IotSetup = null,
                 options = new List<Option>()
             };
             Option o3 = new Option() {option = "Brooklyn Street", question = q2};
@@ -102,6 +115,7 @@ namespace DAL.EF
                 question = "Fill in your e-mail address:",
                 questionnaire = questionnaire1,
                 questionType = QuestionType.MAILADDRESS,
+                IotSetup = null,
                 options = new List<Option>()
             };
             Question q4 = new Question()
@@ -109,6 +123,7 @@ namespace DAL.EF
                 question = "What should we do about the noise polution of the playground?",
                 questionnaire = questionnaire2,
                 questionType = QuestionType.OPEN_QUESTION,
+                IotSetup = new IotSetup(),
                 options = new List<Option>()
             };
             Question q5 = new Question()
@@ -116,18 +131,30 @@ namespace DAL.EF
                 question = "Do you think a playground would be anoying if it was near your home?",
                 questionnaire = questionnaire2,
                 questionType = QuestionType.OPEN_QUESTION,
+                IotSetup = null,
                 options = new List<Option>()
             };
-            questionnaire1.questions = new List<Question>() { q1, q2, q3 };
-            questionnaire2.questions = new List<Question>() { q4, q5 };
-            
+            Question question2 = new Question()
+            {
+                question = "What should we add to the playground?",
+                questionnaire = questionnaire2,
+                questionType = QuestionType.CHECK_BOX,
+                IotSetup = null,
+                options = new List<Option>()
+            };
+            Option option5 = new Option() {option = "Slide", question = question2};
+            Option option6 = new Option() {option = "Drinking fountain", question = question2};
+            Option option7 = new Option() {option = "Swing", question = question2};
+            question2.options = new List<Option>() { option5, option6, option7 };
             // IoTSetups
             IotSetup iot1 = new IotSetup()
             {
                 address = new Address(),
-                //questionnaire = questionnaire1
             };
-          //  questionnaire1.iotSetups = new List<IotSetup>() { iot1 };
+            q4.IotSetup = iot1;
+            
+            questionnaire1.questions = new List<Question>() { q1, q2, q3, question1 };
+            questionnaire2.questions = new List<Question>() { q4, q5, question2 };
             #endregion
             
             #region TestProject 2
@@ -145,10 +172,9 @@ namespace DAL.EF
             Questionnaire questionnaire3 = new Questionnaire()
             {
                 name = "Budget",
-                questionAmount = 2,
+                questionAmount = 4,
                 project = p2,
                 confirmed = true,
-                //iotSetups = new List<IotSetup>(),
                 questions = new List<Question>(),
             };
             p2.questionnaires = new List<Questionnaire>() { questionnaire3 };
@@ -159,6 +185,7 @@ namespace DAL.EF
                 question = "What should we do to save the birds?",
                 questionnaire = questionnaire3,
                 questionType = QuestionType.OPEN_QUESTION,
+                IotSetup = null,
                 options = new List<Option>()
             };
             Question q7 = new Question()
@@ -166,6 +193,7 @@ namespace DAL.EF
                 question = "What do you think should be our first priority?",
                 questionnaire = questionnaire3,
                 questionType = QuestionType.RADIO_BUTTON,
+                IotSetup = null,
                 options = new List<Option>()
             };
             Option o6 = new Option() {option = "Build birds more houses", question = q7};
@@ -173,7 +201,31 @@ namespace DAL.EF
             Option o8 = new Option() {option = "Give more options for birds to drink", question = q7};
             Option o9 = new Option() {option = "Plant more trees", question = q7};
             q7.options = new List<Option>() { o6, o7, o8, o9 };
-            questionnaire3.questions = new List<Question>() { q6, q7 };
+            Question q8 = new Question()
+            {
+                question = "What should we feed the birds?",
+                questionnaire = questionnaire3,
+                questionType = QuestionType.CHECK_BOX,
+                IotSetup = null,
+                options = new List<Option>()
+            };
+            Option o10 = new Option() {option = "Grains", question = q8};
+            Option o11 = new Option() {option = "Seeds", question = q8};
+            Option o12 = new Option() {option = "Chocolat", question = q8};
+            q8.options = new List<Option>() { o10, o11, o12 };
+            Question q9 = new Question()
+            {
+                question = "How should we feed the birds",
+                questionnaire = questionnaire3,
+                questionType = QuestionType.CHECK_BOX,
+                IotSetup = null,
+                options = new List<Option>()
+            };
+            Option o13 = new Option() {option = "With bowls", question = q9};
+            Option o14 = new Option() {option = "On the floor", question = q9};
+            Option o15 = new Option() {option = "Hanging in the tree", question = q9};
+            q9.options = new List<Option>() { o13, o14, o15 };
+            questionnaire3.questions = new List<Question>() { q6, q7, q8, q9 };
             #endregion
             
             //Phases
@@ -195,9 +247,9 @@ namespace DAL.EF
             it1.questions = new List<IdeationQuestion>(){iq1};
             
             //Ideas 
-
-            Idea i1 = new Idea() {ideation = it1, user = u1};
-            Idea i2 = new Idea() {ideation = it1, user = u2};
+            
+            Idea i1 = new Idea(){ideation = it1 , user = u1 };
+            Idea i2 = new Idea(){ideation = it1 , user = u2 };
             
             it1.ideas = new List<Idea>(){ i1,i2};
             
@@ -213,14 +265,9 @@ namespace DAL.EF
             u2.ideas = new List<Idea>(){i2};
             u3.reactions = new List<Reaction>(){a1,a2};
 
-            QuestionUser qu = new QuestionUser() { Question = q1, User = u1, Answer = "Yes"};
-            u1.questionnaireAnswer = new List<QuestionUser>() {qu};
-            q1.questionnaireAnswers = new List<QuestionUser>(){qu};
-
-            context.questionUsers.Add(qu);
             context.projects.AddRange(p1, p2);
             context.questionnaires.AddRange(questionnaire1, questionnaire2, questionnaire3);
-            context.questions.AddRange(q1, q2, q3, q4, q5, q6, q7);
+            context.questions.AddRange(q1, question1, q2, q3, q4, q5, question2, q6, q7, q8, q9);
             context.iotSetups.AddRange(iot1);
             context.phases.AddRange(ph1,ph2);
             context.users.AddRange(u1,u2,u3);
@@ -228,9 +275,8 @@ namespace DAL.EF
             context.ideationQuestions.Add(iq1);
             context.ideas.AddRange(i1,i2);
             context.answers.AddRange(a1,a2);
-            context.options.AddRange(o1, o2, o3, o4, o5, o6, o7, o8, o9);
+            context.options.AddRange(o1, o2, o3, o4, o5, o6, o7, o8, o9, o10, o11, o12, o13, o14, o15, option1, option2, option3, option4, option5, option6, option7);
             
-
             context.SaveChanges();
             
             foreach (var entry in context.ChangeTracker.Entries().ToList())
